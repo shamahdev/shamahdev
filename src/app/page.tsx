5c51af6bccfeacd5e0215dcf8ea4f110280c8786
unknown <shaddam.a.h@gmail.com>
2024-06-23 09:48:45 +0700

    feat: add work experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
               alt="Shaddam Amru Hasibuan"
               width={32}
               height={32}
+              priority
             />
             <div className="flex flex-col py-8 gap-2">
               <h1 className="text-[42px] text-neutral-100 font-bold">
@@ -46,7 +47,7 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-col lg:flex-row gap-4 w-full min-h-[450px]">
-            <div className="bg-neutral-950 border border-neutral-900 rounded-3xl py-6 px-8 flex flex-col justify-between w-full">
+            <div className="bg-neutral-950 border border-neutral-900 rounded-xl py-6 px-8 flex flex-col justify-between w-full">
               <div className="flex flex-col gap-3">
                 <h3 className="font-semibold text-neutral-100">frontend ✨</h3>
                 <p className="text-sm text-neutral-400 leading-loose">
@@ -58,7 +59,7 @@ export default function Home() {
                 </p>
               </div>
             </div>
-            <div className="bg-neutral-950 border border-neutral-900 rounded-3xl py-6 px-8 flex flex-col justify-between w-full">
+            <div className="bg-neutral-950 border border-neutral-900 rounded-xl py-6 px-8 flex flex-col justify-between w-full">
               <div className="flex flex-col gap-3">
                 <h3 className="font-semibold text-neutral-100">ui ux</h3>
                 <p className="text-sm text-neutral-400 leading-loose">
@@ -81,7 +82,7 @@ export default function Home() {
                 </Link>
               </div>
             </div>
-            <div className="bg-neutral-950 border border-neutral-900 rounded-3xl py-6 px-8 flex flex-col justify-between w-full">
+            <div className="bg-neutral-950 border border-neutral-900 rounded-xl py-6 px-8 flex flex-col justify-between w-full">
               <div className="flex flex-col gap-3">
                 <h3 className="font-semibold text-neutral-100">vr/ar</h3>
                 <p className="text-sm text-neutral-400 leading-loose">
@@ -98,22 +99,25 @@ export default function Home() {
                   alt="Nestle"
                   width={63.5}
                   height={40}
+                  priority
                 />
                 <Image
                   src="/images/ar-sentral.png"
                   alt="Sentral Tunjungan"
                   width={66.5}
                   height={40}
+                  priority
                 />
                 <Image
                   src="/images/ar-wyeth.png"
                   alt="Wyeth Nutrition"
                   width={135}
                   height={40}
+                  priority
                 />
               </div>
             </div>
-            <div className="bg-neutral-100 rounded-3xl py-6 px-8 flex flex-col justify-between w-full">
+            <div className="bg-neutral-100 rounded-xl py-6 px-8 flex flex-col justify-between w-full">
               <div className="flex flex-col gap-3">
                 <h3 className="font-semibold text-neutral-950">brand design</h3>
                 <p className="text-sm text-neutral-700 leading-loose">
@@ -130,31 +134,218 @@ export default function Home() {
                   alt="Smeshub"
                   width={85.5}
                   height={40}
+                  priority
                 />
                 <Image
                   src="/images/brand-mantenkuh.png"
                   alt="Mantenkuh"
                   width={106}
                   height={40}
+                  priority
                 />
                 <Image
                   src="/images/brand-siomay.png"
                   alt="Somay Lonjong"
                   width={58.5}
                   height={40}
+                  priority
                 />
                 <Image
                   src="/images/brand-printingmart.png"
                   alt="printingmart"
                   width={114.5}
                   height={40}
+                  priority
                 />
                 <Image
                   src="/images/brand-artire.png"
                   alt="Smeshub"
                   width={70}
                   height={40}
+                  priority
                 />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg font-semibold text-neutral-100">
+              work experiences
+            </h2>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full">
+              <div className="flex flex-col gap-4 w-full">
+                <div className="bg-neutral-950 border border-neutral-900 rounded-xl py-6 px-8 flex flex-col justify-between w-full gap-8">
+                  <Image
+                    src="/images/work-salt.png"
+                    alt="Salt Indonesia"
+                    width={50}
+                    height={24}
+                    priority
+                  />
+                  <div className="flex flex-col gap-3">
+                    <h3 className="text-neutral-100">
+                      <b>salt</b> / 2023 - present
+                    </h3>
+                    <p className="text-sm text-neutral-400 leading-loose">
+                      Brought on board for SALT new branch in Bandung as a mid
+                      level front-end engineer.
+                    </p>
+                    <p className="text-sm text-neutral-400 leading-loose">
+                      Worked using React.js for coding and take care of code
+                      quality and deployment reliability by creating unit tests.
+                      Trusted by companies to work as an outsource in renowned
+                      companies like Telkomsel.
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-neutral-100 rounded-xl py-6 px-8 flex flex-col justify-between w-full gap-8">
+                  <div className="flex gap-4 items-center">
+                    <Image
+                      src="/images/work-bdd.png"
+                      alt="BDD"
+                      width={50}
+                      height={24}
+                      priority
+                    />
+                    <Image
+                      src="/images/work-kampusmerdeka.png"
+                      alt="Kampus Merdeka x Dicoding"
+                      width={130}
+                      height={24}
+                      priority
+                    />
+                    <Image
+                      src="/images/work-studidevsecops.png"
+                      alt="Studidevsecops"
+                      width={90}
+                      height={24}
+                      priority
+                    />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <h3 className="text-neutral-950">
+                      <b>speaking & mentoring</b> / 2022 - present
+                    </h3>
+                    <p className="text-sm text-neutral-700 leading-loose">
+                      I speak and facilitate workshop on several programs within
+                      frontend and project management expertise in collaboration
+                      with Dicoding.
+                    </p>
+                    <p className="text-sm text-neutral-700 leading-loose">
+                      Assist in creating a workshop syllabus for frontend
+                      development at Studidevsecops.
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#15304E] rounded-xl py-6 px-8 flex flex-col justify-between w-full gap-8 relative overflow-hidden">
+                  <Image
+                    src="/images/work-dicoding.png"
+                    alt="Dicoding"
+                    width={100}
+                    height={24}
+                    priority
+                  />
+                  <div className="flex flex-col gap-3">
+                    <h3 className="text-neutral-100">
+                      <b>dicoding</b> / 2020 - present
+                    </h3>
+                    <p className="text-sm text-neutral-200 leading-loose">
+                      As an External Code Reviewer, I have successfully provided
+                      technical assistance and reviewed over 1200 project
+                      submissions from students.
+                    </p>
+                    <p className="text-sm text-neutral-200 leading-loose">
+                      I have helped many individuals graduate from
+                      Dicoding&apos;s courses, earning high ratings from
+                      satisfied students.
+                    </p>
+                  </div>
+                  <span className="h-[240px]" />
+                  <div className="flex w-full gap-2 absolute -bottom-8 -right-16">
+                    <Image
+                      src="/images/dicoding/cer-1.png"
+                      alt="Dicoding Academy Reviewer Certificate 2020"
+                      width={360}
+                      height={240}
+                      priority
+                    />
+                    <Image
+                      src="/images/dicoding/cer-2.png"
+                      alt="Dicoding Academy Reviewer Certificate 2021"
+                      width={360}
+                      height={240}
+                      priority
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col w-full gap-4">
+                <div className="bg-[#E3571B] rounded-xl py-6 px-8 flex flex-col justify-between w-full gap-8 relative overflow-hidden">
+                  <Image
+                    src="/images/work-smeshub.png"
+                    alt="Smeshub"
+                    width={100}
+                    height={24}
+                    priority
+                  />
+                  <div className="flex flex-col gap-3">
+                    <h3 className="text-neutral-100">
+                      <b>smeshub</b> / 2021 - 2023
+                    </h3>
+                    <p className="text-sm text-neutral-200 leading-loose">
+                      As the first engineering hired at Smeshub, I turned the
+                      founder&apos;s vision into a user-friendly MVP through
+                      full stack development.
+                    </p>
+                    <p className="text-sm text-neutral-200 leading-loose">
+                      Later, after our team grew, I assigned as a Frontend
+                      Engineer, I led a team and delivering high-quality
+                      front-end solutions.
+                    </p>
+                  </div>
+                  <span className="h-[400px]" />
+                  <div className="flex w-full gap-2 absolute -bottom-16 left-0 right-0 items-end justify-center">
+                    <Image
+                      src="/images/smeshub/web-1.png"
+                      alt="Smeshub Website"
+                      width={300}
+                      height={300}
+                      priority
+                    />
+                    <Image
+                      src="/images/smeshub/web-2.png"
+                      alt="Smeshub Website"
+                      width={300}
+                      height={400}
+                      priority
+                    />
+                    <Image
+                      src="/images/smeshub/web-3.png"
+                      alt="Smeshub Website"
+                      width={300}
+                      height={300}
+                      priority
+                    />
+                  </div>
+                </div>
+                <div className="bg-neutral-950 border border-neutral-900 rounded-xl py-6 px-8 flex flex-col justify-between w-full gap-8">
+                  <Image
+                    src="/images/work-linxt.png"
+                    alt="Linxt Studio"
+                    width={80}
+                    height={24}
+                    priority
+                  />
+                  <div className="flex flex-col gap-3">
+                    <h3 className="text-neutral-100">
+                      <b>linxt studio</b> / 2023 - present
+                    </h3>
+                    <p className="text-sm text-neutral-400 leading-loose">
+                      I founded a developer studio with purpose of collaborating
+                      on multiple web and application projects, as well as
+                      creating open-source innovation.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -166,30 +357,9 @@ export default function Home() {
           </div>
         </div>
       </Container>
-      <div className="relative overflow-hidden mt-24 whitespace-nowrap opacity-5 select-none">
-        <div className="w-full flex gap-8 items-center animate-marquee">
-          <p className="font-bold text-[96px] text-neutral-100 ml-8">
-            SAY HELLO!
-          </p>
-          <p className="text-[96px] text-neutral-100">ハイって言って!!</p>
-          <p className="font-bold text-[96px] text-neutral-100">
-            GET IN TOUCH!
-          </p>
-          <p className="text-[96px] text-neutral-100">ハイって言って!!</p>
-        </div>
-        <div className="absolute top-0 w-full flex gap-8 items-center animate-marquee-end">
-          <p className="font-bold text-[96px] text-neutral-100 ml-8">
-            SAY HELLO!
-          </p>
-          <p className="text-[96px] text-neutral-100">ハイって言って!!</p>
-          <p className="font-bold text-[96px] text-neutral-100">
-            GET IN TOUCH!
-          </p>
-          <p className="text-[96px] text-neutral-100">ハイって言って!!</p>
-        </div>
-      </div>
+
       <Container>
-        <footer className="w-full flex flex-col gap-4 py-12">
+        <footer className="w-full flex flex-col gap-4 py-12 pt-24">
           <p className="font-bold text-xl text-neutral-100">hello@shamah.dev</p>
           <p className="text-sm text-neutral-400 leading-loose max-w-3xl">
             Open for new opportunity and project-based works, Feel free to inbox
--- src/common/components/Carousel.tsx
+++ src/common/components/Carousel.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import {
-  DotButton,
-  useDotButton,
-} from "@/common/components/CarouselDotButtons";
-import clsx from "clsx";
-import useEmblaCarousel from "embla-carousel-react";
 import { ExternalLinkIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
 
 function ProjectCard({
   title,
@@ -25,7 +18,7 @@ function ProjectCard({
   url: string;
 }) {
   return (
-    <div className="bg-neutral-[#131313] hover:bg-neutral-950 transition-colors border border-neutral-900 rounded-3xl py-6 px-8 flex flex-col justify-between w-full select-none">
+    <div className="bg-neutral-[#131313] hover:bg-neutral-950 transition-colors border border-neutral-900 rounded-xl py-6 px-8 flex flex-col justify-between w-full select-none">
       <div className="flex flex-col gap-3">
         <Link
           href={url}
@@ -55,92 +48,59 @@ function ProjectCard({
 }
 
 export function Carousel() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
-
-  const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi);
-
-  useEffect(() => {
-    if (emblaApi) {
-      console.log(emblaApi.slideNodes()); // Access API
-    }
-  }, [emblaApi]);
-
   return (
-    <div className="embla overflow-hidden" ref={emblaRef}>
-      <div className="embla__container flex gap-4 cursor-grab">
-        <div className="embla__slide flex-[0_0_33%] min-w-0">
-          <ProjectCard
-            title="localvocal"
-            description="Localvocal is a local brand e-commerce site made with Medusa and Next.js"
-            year="2023"
-            url="https://github.com/localvocalid"
-            icons={[
-              "/images/icon/ic-medusa.png",
-              "/images/icon/ic-nextjs.png",
-              "/images/icon/ic-react.png",
-              "/images/icon/ic-tailwind.png",
-              "/images/icon/ic-postgres.png",
-              "/images/icon/ic-digitalocean.png",
-            ]}
-          />
-        </div>
-        <div className="embla__slide flex-[0_0_33%] min-w-0">
-          <ProjectCard
-            title="dokter rasa"
-            description="Yet another restaurant list progressive web app with review and bookmark feature."
-            year="2021"
-            url="https://dokterrasa.web.app/"
-            icons={[
-              "/images/icon/ic-webpack.png",
-              "/images/icon/ic-pwa.png",
-              "/images/icon/ic-sass.png",
-              "/images/icon/ic-jasmine.png",
-              "/images/icon/ic-karma.png",
-            ]}
-          />
-        </div>
-        <div className="embla__slide flex-[0_0_33%] min-w-0">
-          <ProjectCard
-            title="celenganku"
-            description="Students finance management or money saving progressive web app."
-            year="2021"
-            url="https://github.com/shamahdev/celenganku"
-            icons={[
-              "/images/icon/ic-firebase.png",
-              "/images/icon/ic-webpack.png",
-              "/images/icon/ic-pwa.png",
-              "/images/icon/ic-tailwind.png",
-              "/images/icon/ic-midtrans.png",
-            ]}
-          />
-        </div>
-        <div className="embla__slide flex-[0_0_33%] min-w-0">
-          <ProjectCard
-            title="dokter kimo"
-            description="Game-based learning mobile app for Kihajar Mobile App Competition 2020.."
-            year="2020"
-            url="https://github.com/shamahdev/dokter-kimo"
-            icons={[
-              "/images/icon/ic-unity.png",
-              "/images/icon/ic-csharp.png",
-              "/images/icon/ic-animate.png",
-            ]}
-          />
-        </div>
-      </div>
-      <div className="flex items-center gap-2 pt-8">
-        {scrollSnaps.map((_, index) => (
-          <DotButton
-            key={index}
-            onClick={() => onDotButtonClick(index)}
-            className={clsx(
-              "w-2 h-2 rounded-full bg-neutral-50",
-              index === selectedIndex ? "opacity-100" : "opacity-50"
-            )}
-          />
-        ))}
-      </div>
+    <div className="gap-4 grid grid-cols-3">
+      <ProjectCard
+        title="localvocal"
+        description="Localvocal is a local brand e-commerce site made with Medusa and Next.js"
+        year="2023"
+        url="https://github.com/localvocalid"
+        icons={[
+          "/images/icon/ic-medusa.png",
+          "/images/icon/ic-nextjs.png",
+          "/images/icon/ic-react.png",
+          "/images/icon/ic-tailwind.png",
+          "/images/icon/ic-postgres.png",
+          "/images/icon/ic-digitalocean.png",
+        ]}
+      />
+      <ProjectCard
+        title="dokter rasa"
+        description="Yet another restaurant list progressive web app with review and bookmark feature."
+        year="2021"
+        url="https://dokterrasa.web.app/"
+        icons={[
+          "/images/icon/ic-webpack.png",
+          "/images/icon/ic-pwa.png",
+          "/images/icon/ic-sass.png",
+          "/images/icon/ic-jasmine.png",
+          "/images/icon/ic-karma.png",
+        ]}
+      />
+      <ProjectCard
+        title="celenganku"
+        description="Students finance management or money saving progressive web app."
+        year="2021"
+        url="https://github.com/shamahdev/celenganku"
+        icons={[
+          "/images/icon/ic-firebase.png",
+          "/images/icon/ic-webpack.png",
+          "/images/icon/ic-pwa.png",
+          "/images/icon/ic-tailwind.png",
+          "/images/icon/ic-midtrans.png",
+        ]}
+      />
+      <ProjectCard
+        title="dokter kimo"
+        description="Game-based learning mobile app for Kihajar Mobile App Competition 2020.."
+        year="2020"
+        url="https://github.com/shamahdev/dokter-kimo"
+        icons={[
+          "/images/icon/ic-unity.png",
+          "/images/icon/ic-csharp.png",
+          "/images/icon/ic-animate.png",
+        ]}
+      />
     </div>
   );
 }
